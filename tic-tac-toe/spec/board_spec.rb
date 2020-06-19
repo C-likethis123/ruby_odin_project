9aa867db52c807ba173c60038dfa48db5a523ceb
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-19 15:56:10 +0800

    Write test code for Board#new

--- tic-tac-toe/spec/board_spec.rb
+++ tic-tac-toe/spec/board_spec.rb
@@ -1,16 +1,11 @@
-require "board"
-RSpec.describe Board do
-    describe '#new' do
-        it 'creates a new 3x3 grid' do
-            expect(Board.new.board).to eq([[1,2,3], [4,5,6], [7,8,9]])
-        end
-    end
+# frozen_string_literal: true
 
-    # describe '#has_streak' do
-    #     it 'returns true if there is a horizontal streak' do
-    #         new_board = Board.new
-    #         board = ["X", "X", "X", [4,5,6], [7,8,9]]
-    #         expect()
-    #     end
-    # end
+require 'board'
+RSpec.describe Board do
+  describe '#new' do
+    it 'creates a new 3x3 grid' do
+      expect(Board.new.board).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    end
+  end
+
 end
