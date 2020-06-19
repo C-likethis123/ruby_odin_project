5864d8cdad0abd00cf126ac41926252363f93d80
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-19 18:07:13 +0800

    Add #streak? to check if there is a streak in the board

--- tic-tac-toe/lib/board.rb
+++ tic-tac-toe/lib/board.rb
@@ -21,4 +21,25 @@ class Board
   def add_to_grid(player_symbol, position)
     @board[position - 1] = player_symbol
   end
+
+  def streak?
+    # checks horizonal streaks
+    (0..2).each do |row|
+      first_element = board[row * 3]
+      return true if board[row * 3 + 1] == first_element && board[row * 3 + 2] == first_element
+    end
+
+    # checks vertical streaks
+    (0..2).each do |column|
+      first_element = board[column]
+      return true if board[column + 3] == first_element && board[column + 6] == first_element
+    end
+
+    # checks diagonals
+    return true if board[0] == board[4] && board[0] == board[8]
+
+    return true if board[2] == board[4] && board[2] == board[6]
+
+    false
+  end
 end
--- tic-tac-toe/spec/board_spec.rb
+++ tic-tac-toe/spec/board_spec.rb
@@ -18,4 +18,30 @@ RSpec.describe Board do
       expect(new_board.board).to eq(['X', 2, 3, 4, 5, 6, 'O', 8, 9])
     end
   end
+
+  describe '#streak?' do
+    it 'returns true when there is a horizontal streak' do
+      new_board = Board.new
+      new_board.add_to_grid('X', 1)
+      new_board.add_to_grid('X', 2)
+      new_board.add_to_grid('X', 3)
+      expect(new_board.streak?).to eq(true)
+    end
+
+    it 'returns true when there is a vertical streak' do
+      new_board = Board.new
+      new_board.add_to_grid('X', 1)
+      new_board.add_to_grid('X', 4)
+      new_board.add_to_grid('X', 7)
+      expect(new_board.streak?).to eq(true)
+    end
+
+    it 'returns true when there is a diagonal streak' do
+      new_board = Board.new
+      new_board.add_to_grid('X', 1)
+      new_board.add_to_grid('X', 5)
+      new_board.add_to_grid('X', 9)
+      expect(new_board.streak?).to eq(true)
+    end
+  end
 end
