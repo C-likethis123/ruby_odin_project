64252d93651d371ebd5a2080e617a02f954b4f24
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-19 16:25:34 +0800

    Add #add_to_grid method

--- tic-tac-toe/lib/board.rb
+++ tic-tac-toe/lib/board.rb
@@ -3,6 +3,22 @@
 class Board
   attr_reader :board
   def initialize
-    @board = [[*(1..3)], [*(4..6)], [*(7..9)]]
+    @board = [*(1..9)]
+  end
+
+  def print_board
+    board_string = "/---|---|---\\\n" \
+                   "| #{board[0]} | #{board[1]} | #{board[2]} |\n" \
+                   "|-----------|\n" \
+                   "| #{board[3]} | #{board[4]} | #{board[5]} |\n" \
+                   "|-----------|\n" \
+                   "| #{board[6]} | #{board[7]} | #{board[8]} |\n" \
+                   "/---|---|---\\\n"
+
+    puts board_string
+  end
+
+  def add_to_grid(player_symbol, position)
+    @board[position - 1] = player_symbol
   end
 end
--- tic-tac-toe/spec/board_spec.rb
+++ tic-tac-toe/spec/board_spec.rb
@@ -4,8 +4,18 @@ require 'board'
 RSpec.describe Board do
   describe '#new' do
     it 'creates a new 3x3 grid' do
-      expect(Board.new.board).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+      expect(Board.new.board).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
   end
 
+  describe '#add_to_grid' do
+    it 'updates the board array' do
+      new_board = Board.new
+      new_board.add_to_grid('X', 1)
+      expect(new_board.board).to eq(['X', 2, 3, 4, 5, 6, 7, 8, 9])
+
+      new_board.add_to_grid('O', 7)
+      expect(new_board.board).to eq(['X', 2, 3, 4, 5, 6, 'O', 8, 9])
+    end
+  end
 end
