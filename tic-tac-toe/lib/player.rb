ab1edd0120e2537825d9a1c710c6fa6a87f5e814
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-19 21:25:40 +0800

    Add #makes_move method

---
+++ tic-tac-toe/lib/player.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Player
+  def initialize(player_symbol)
+    @player_symbol = player_symbol
+  end
+
+  def makes_move(board)
+    board.print_board
+    puts 'Enter the grid position you want to make your move in:'
+    position = gets.to_i
+    board.add_to_grid(@player_symbol, position)
+  end
+end
---
+++ tic-tac-toe/spec/player_spec.rb
@@ -0,0 +1,24 @@
+require 'player'
+require 'board'
+RSpec.describe Player do
+  describe '#makes_move' do
+    it 'updates the board array' do
+      new_board = Board.new
+      new_player = Player.new('X')
+
+      orig_stdin = $stdin
+      $stdin = StringIO.new('1')
+      
+      new_player.makes_move(new_board)
+      expect(new_board.board).to eq(['X', 2, 3, 4, 5, 6, 7, 8, 9])
+      $stdin = orig_stdin
+
+      player_O = Player.new('O')
+      orig_stdin = $stdin
+      $stdin = StringIO.new('7')
+      player_O.makes_move(new_board)
+      expect(new_board.board).to eq(['X', 2, 3, 4, 5, 6, 'O', 8, 9])
+      $stdin = orig_stdin
+    end
+  end
+end
