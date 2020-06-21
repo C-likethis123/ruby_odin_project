052f6035a011a1e474e2bf95bfbf3816b7c39fb4
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-21 15:39:35 +0800

    Make clean_zipcode more succinct

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -3,19 +3,7 @@
 require 'csv'
 
 def clean_zipcode(zipcode)
-  # if the zip code is exactly five digits, assume that it is ok
-  # if the zip code is not specified, return a string of zeros
-  if zipcode.nil?
-    '00000'
-    # if the zip code is more than 5 digits, truncate it to the first 5 digits
-  elsif zipcode.length > 5
-    zipcode[0..4]
-    # if the zip code is less than 5 digits, add zeros to the front until it becomes five digits
-  elsif zipcode.length < 5
-    zipcode.rjust(5, '0')
-  else
-    zipcode
-  end
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
 puts 'EventManager initialized.'
