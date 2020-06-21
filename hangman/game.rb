1ed3b182723f475b8cd0fc26588fa44977bfa35c
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-21 23:04:39 +0800

    Refactor logic to a Game class

--- hangman/game.rb
+++ hangman/game.rb
@@ -1,53 +1,56 @@
 # frozen_string_literal: true
 
-def select_word
-  contents = File.readlines('words.txt')
-  selected_word = contents.sample
-  selected_word = contents.sample while selected_word.length < 5 || selected_word.length > 12
-  selected_word.chomp.downcase
-end
+class Game
+  def select_word
+    contents = File.readlines('words.txt')
+    selected_word = contents.sample
+    selected_word = contents.sample while selected_word.length < 5 || selected_word.length > 12
+    selected_word.chomp.downcase
+    end
 
-def initialize_game
-  $word = select_word.split(//)
-  $tries = 10
-  $blanks = ['_'] * $word.length
-end
+  def initialize_game
+    @word = select_word.split(//)
+    @tries = 10
+    @blanks = ['_'] * $word.length
+    end
 
-def game_not_over
-  $blanks.any? { |blank| blank == '_' } && $tries > 0
-end
-
-def guess_incorrect(user_guess)
-  $word.none? { |word| word == user_guess }
-end
-
-def replace_blanks_with_user_guess(user_guess)
-  $word.each_with_index do |letter, index|
-    $blanks[index] = user_guess if letter == user_guess
+  def game_not_over
+    $blanks.any? { |blank| blank == '_' } && $tries > 0
   end
-end
 
-def join_word(word)
-  word.join('')
-end
-
-initialize_game
-
-while game_not_over
-  puts $blanks.to_s
-  print 'Guess a letter: '
-  user_guess = gets.chomp.downcase
-  if guess_incorrect(user_guess)
-    $tries -= 1
-    puts "You've guessed wrongly! You have #{$tries} tries left\n"
-  else
-    puts "You've guessed correctly!"
-    replace_blanks_with_user_guess(user_guess)
+  def guess_incorrect(user_guess)
+    $word.none? { |word| word == user_guess }
   end
-end
 
-if $tries == 0
-  puts "You lost! The word is #{join_word($word)}"
-else
-  puts "You won! The word is #{join_word($word)}. Play again?"
+  def replace_blanks_with_user_guess(user_guess)
+    $word.each_with_index do |letter, index|
+      $blanks[index] = user_guess if letter == user_guess
+    end
+  end
+
+  def join_word(word)
+    word.join('')
+  end
+
+  def play
+    initialize_game
+    while game_not_over
+      puts $blanks.to_s
+      print 'Guess a letter: '
+      user_guess = gets.chomp.downcase
+      if guess_incorrect(user_guess)
+        $tries -= 1
+        puts "You've guessed wrongly! You have #{$tries} tries left\n"
+      else
+        puts "You've guessed correctly!"
+        replace_blanks_with_user_guess(user_guess)
+      end
+      end
+
+    if $tries == 0
+      puts "You lost! The word is #{join_word($word)}"
+    else
+      puts "You won! The word is #{join_word($word)}. Play again?"
+    end
+    end
 end
