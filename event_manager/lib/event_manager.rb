f204745234ba4127d2ecb99a7f7ddf9eb79a3553
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-21 15:21:03 +0800

    Iteration 1: Using CVS library

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,11 +1,9 @@
-puts "EventManager initialised!"
+require "csv"
+puts "EventManager initialized."
 
-# Iteration 0
-# We can read a file line by line, and use next if to skip the first iteration.
-contents = File.readlines './event_attendees.csv'
-contents.each_with_index do |line, index|
-    next if index == 0
-    column = line.split(",")
-    name = column[2]
-    puts name
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name}: #{zipcode}"
 end
