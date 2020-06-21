c08e2deb14c4f29afd9063d22213cdc3edd14ae8
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-21 23:57:58 +0800

    Refactor #initialize_game to #initialize

--- hangman/game.rb
+++ hangman/game.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class Game
+  def initialize
+    @word = select_word.split(//)
+    @tries = 10
+    @blanks = ['_'] * @word.length
+  end
+
   def select_word
     contents = File.readlines('words.txt')
     selected_word = contents.sample
     selected_word = contents.sample while selected_word.length < 5 || selected_word.length > 12
     selected_word.chomp.downcase
-  end
-
-  def initialize_game
-    @word = select_word.split(//)
-    @tries = 10
-    @blanks = ['_'] * @word.length
   end
 
   def save_game
@@ -47,7 +47,6 @@ end
   end
 
   def play
-    initialize_game
     while game_not_over
       puts @blanks.to_s
       print 'Guess a letter: '
