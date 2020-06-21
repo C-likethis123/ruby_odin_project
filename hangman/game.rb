aec00a52062320c3920826feb2d03070d6df3435
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-21 22:50:40 +0800

    Remove whitespace when a random word is selected

--- hangman/game.rb
+++ hangman/game.rb
@@ -4,7 +4,7 @@ def select_word
   contents = File.readlines('words.txt')
   selected_word = contents.sample
   selected_word = contents.sample while selected_word.length < 5 || selected_word.length > 12
-  selected_word
+  selected_word.chomp.downcase
 end
 
 def initialize_game
