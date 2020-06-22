f6e02ef8f0ef8b839f91ad4c1e8b9bda2515ea92
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-22 21:10:14 +0800

    Reorder methods in LinkedList

--- data_structures/linked_list.rb
+++ data_structures/linked_list.rb
@@ -9,12 +9,15 @@ class LinkedList
         @tail = @head
     end
 
-    def head
-        @head
+    def append(value)
+        new_node = Node.new(value)
+        @tail.next_node = new_node
+        @tail = new_node
     end
 
-    def tail
-        @tail
+    def prepend(value)
+        new_node = Node.new(value, @head.next_node)
+        @head.next_node = new_node
     end
 
     def size
@@ -27,15 +30,12 @@ class LinkedList
         size_of_node
     end
 
-    def prepend(value)
-        new_node = Node.new(value, @head.next_node)
-        @head.next_node = new_node
+    def head
+        @head
     end
 
-    def append(value)
-        new_node = Node.new(value)
-        @tail.next_node = new_node
-        @tail = new_node
+    def tail
+        @tail
     end
 
     def to_s
@@ -48,3 +48,23 @@ class LinkedList
         res += "nil"
     end
 end
+
+# # test cases
+
+# size
+linked_list = LinkedList.new
+puts linked_list.size == 0
+
+# append
+linked_list = LinkedList.new
+linked_list.append(5)
+puts linked_list.size == 1
+linked_list.append(9)
+puts linked_list.size == 2
+
+# prepend
+linked_list.prepend(2)
+puts linked_list.to_s == "( 2 ) -> ( 5 ) -> ( 9 ) -> nil"
+
+# to_s
+puts linked_list.to_s
