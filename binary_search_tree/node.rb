075b34fb034f9b3d5cccf10d8ebcaa75a8eca407
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-22 23:54:47 +0800

    Format code

--- binary_search_tree/node.rb
+++ binary_search_tree/node.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 # Represents a Node in a Binary Search Tree
 class Node
-    include Comparable
-    attr_accessor :value, :left, :right
-    def initialize(value, left, right)
-        @value = value
-        @left = @left
-        @right = @right
-    end
+  include Comparable
+  attr_accessor :value, :left, :right
+  def initialize(value, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
 
-    def <=>(other)
-        @value <=> other.value
-    end
+  def <=>(other)
+    @value <=> other.value
+  end
 end
