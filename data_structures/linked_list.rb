889363f207f5a92e1ddea5831ceadf2fde6d009d
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-22 20:59:59 +0800

    Change implementation of size

--- data_structures/linked_list.rb
+++ data_structures/linked_list.rb
@@ -20,9 +20,25 @@ class LinkedList
     def size
         size_of_node = 0
         node = @head.next_node
-        unless node.nil?
+        while node
             node = node.next_node
             size_of_node += 1
         end
         size_of_node
     end
+
+    def append(value)
+        new_node = Node.new(value)
+        tail.next_node = new_node
+    end
+end
+
+# test cases
+
+# size
+linked_list = LinkedList.new
+# linked_list.append(5)
+puts linked_list.size == 0
+        res += "nil"
+    end
+end
