6f9164239faca3cb307c171221430fa711c4206f
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-22 22:05:58 +0800

    Create a Node class

---
+++ binary_search_tree/node.rb
@@ -0,0 +1,14 @@
+# Represents a Node in a Binary Search Tree
+class Node
+    include Comparable
+    attr_accessor :value, :left, :right
+    def initialize(value, left, right)
+        @value = value
+        @left = @left
+        @right = @right
+    end
+
+    def <=>(other)
+        @value <=> other.value
+    end
+end
