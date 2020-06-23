c16e8a2426d6b1af16f8645672f7178921c75e27
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-23 23:15:05 +0800

    Allow rspec to run failing tests only

--- tic-tac-toe/spec/spec_helper.rb
+++ tic-tac-toe/spec/spec_helper.rb
@@ -39,6 +39,7 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.example_status_persistence_file_path = "failures.txt"
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
@@ -58,7 +59,7 @@ RSpec.configure do |config|
   #   # Allows RSpec to persist some state between runs in order to support
   #   # the `--only-failures` and `--next-failure` CLI options. We recommend
   #   # you configure your source control system to ignore this file.
-  #   config.example_status_persistence_file_path = "spec/examples.txt"
+    config.example_status_persistence_file_path = "spec/examples.txt"
   #
   #   # Limits the available syntax to the non-monkey patched syntax that is
   #   # recommended. For more details, see:
