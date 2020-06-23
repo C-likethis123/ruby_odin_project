4050cb77b974e6836b0df228082b324e4cc5bff6
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-23 17:28:38 +0800

    Remove Caesar class

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -6,33 +6,31 @@
 65-90: "A" to "Z"
 97-122: "a" to "z"
 =end
-class Caesar
-    def is_punctuation(letter)
-        ascii = letter.ord
-        return ascii < 65 || (ascii > 90 && ascii < 97) || ascii > 122
+def is_punctuation(letter)
+    ascii = letter.ord
+    return ascii < 65 || (ascii > 90 && ascii < 97) || ascii > 122
+end
+
+def get_letter_within_range(letter, shift_factor)
+    if is_punctuation(letter)
+        return letter
     end
 
-    def get_letter_within_range(letter, shift_factor)
-        if is_punctuation(letter)
-            return letter
-        end
-
-        ascii = letter.ord + shift_factor
-        if (ascii > 90 && ascii < 97)
-            return ((ascii % 90) + 64).chr
-        elsif (ascii > 122)
-            return ((ascii % 122) + 96).chr
-        else
-            return ascii.chr
-        end
+    ascii = letter.ord + shift_factor
+    if (ascii > 90 && ascii < 97)
+        return ((ascii % 90) + 64).chr
+    elsif (ascii > 122)
+        return ((ascii % 122) + 96).chr
+    else
+        return ascii.chr
     end
+end
 
-    def caesar_cipher(string, shift_factor)
-        result = ""
-        string = string.split(//)
-        for letter in string
-            result += (get_letter_within_range(letter, shift_factor))
-        end
-        return result
+def caesar_cipher(string, shift_factor)
+    result = ""
+    string = string.split(//)
+    for letter in string
+        result += (get_letter_within_range(letter, shift_factor))
     end
+    return result
 end
